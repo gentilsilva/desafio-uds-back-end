3e30665a5181bb2762cf3a0280bb049914334bf2
Gentil Silva <silvagentil.sg@gmail.com>
2023-10-11 20:05:52 -0300

    feat: Add input data validation

--- desafioudstecnologia/src/main/java/com/desafioudstecnologia/dtos/ProductForm.java
+++ desafioudstecnologia/src/main/java/com/desafioudstecnologia/dtos/ProductForm.java
@@ -1,15 +1,18 @@
 package com.desafioudstecnologia.dtos;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 
 import java.math.BigDecimal;
 
 public record ProductForm(
+        @NotBlank
         String code,
         @NotBlank
         String name,
         @Positive
+        @NotNull
         BigDecimal unitaryPrice
 ) {
 }
