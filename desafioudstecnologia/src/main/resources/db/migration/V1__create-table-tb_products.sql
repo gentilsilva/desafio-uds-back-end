d0ddb6d5ee35dc464e266d4ead4c0f3c41f35023
Gentil Silva <silvagentil.sg@gmail.com>
2023-10-11 21:47:40 -0300

    feat: Add primary key in V1 migration

--- desafioudstecnologia/src/main/resources/db/migration/V1__create-table-tb_products.sql
+++ desafioudstecnologia/src/main/resources/db/migration/V1__create-table-tb_products.sql
@@ -2,5 +2,7 @@ CREATE TABLE tb_products(
     id UUID DEFAULT RANDOM_UUID() NOT NULL,
     code VARCHAR(255) NOT NULL UNIQUE,
     name VARCHAR(255) NOT NULL UNIQUE,
-    unitary_price DECIMAL(10, 2)
+    unitary_price DECIMAL(10, 2),
+
+    PRIMARY KEY(id)
 )
