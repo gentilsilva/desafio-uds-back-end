2365236c9c69dd7d2c77101c563741f14dcfcc17
Gentil Silva <silvagentil.sg@gmail.com>
2023-10-13 16:37:28 -0300

    feat: Created Order controller class

---
+++ desafioudstecnologia/src/main/java/com/desafioudstecnologia/controllers/OrderController.java
@@ -0,0 +1,17 @@
+package com.desafioudstecnologia.controllers;
+
+import com.desafioudstecnologia.services.OrderService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/orders")
+public class OrderController {
+
+    private final OrderService orderService;
+
+    public OrderController(OrderService orderService) {
+        this.orderService = orderService;
+    }
+
+}
