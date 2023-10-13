e01d6c8eca5ea78c8f25c0f54809caca4e64597b
Gentil Silva <silvagentil.sg@gmail.com>
2023-10-13 16:31:15 -0300

    feat: V5 migration to tb_orders

---
+++ desafioudstecnologia/src/main/resources/db/migration/V5__create-table-tb_orders.sql
@@ -0,0 +1,10 @@
+CREATE TABLE tb_orders(
+    id UUID DEFAULT RANDOM_UUID() NOT NULL UNIQUE,
+    number INTEGER NOT NULL UNIQUE,
+    emission DATE NOT NULL,
+    total DECIMAL(10, 2) NOT NULL,
+    client_cpf VARCHAR(11) NOT NULL UNIQUE,
+
+    PRIMARY KEY(id),
+    FOREIGN KEY(client_cpf) REFERENCES tb_clients(cpf)
+)
