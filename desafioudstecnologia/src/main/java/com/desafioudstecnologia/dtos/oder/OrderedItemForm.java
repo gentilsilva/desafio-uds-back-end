f40df9beb080a64ab3e1d81a56e18ae739c56cd6
Gentil Silva <silvagentil.sg@gmail.com>
2023-10-14 06:22:09 -0300

    feat: Adding validation to OrderedItemForm

--- desafioudstecnologia/src/main/java/com/desafioudstecnologia/dtos/oder/OrderedItemForm.java
+++ desafioudstecnologia/src/main/java/com/desafioudstecnologia/dtos/oder/OrderedItemForm.java
@@ -1,8 +1,15 @@
 package com.desafioudstecnologia.dtos.oder;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.PositiveOrZero;
+
 public record OrderedItemForm(
+        @NotBlank
         String code,
+        @Positive
         Integer amount,
+        @PositiveOrZero
         Double discountPercentage
 ) {
 }
