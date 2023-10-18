ee506b1a3c89e33e7c968fbcb4f5839527c71bfb
Gentil Silva <silvagentil.sg@gmail.com>
2023-10-18 02:55:39 -0300

    refactor: add MethodArgumentInvalidException handler method

--- desafioudstecnologia/src/main/java/com/desafioudstecnologia/controllers/GlobalControllerAdvice.java
+++ desafioudstecnologia/src/main/java/com/desafioudstecnologia/controllers/GlobalControllerAdvice.java
@@ -1,25 +1,33 @@
 package com.desafioudstecnologia.controllers;
 
 import com.desafioudstecnologia.exeptions.DuplicateRecordException;
+import com.desafioudstecnologia.exeptions.InvalidArgumentException;
 import com.desafioudstecnologia.exeptions.RecordNotFoundException;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
 
 @RestControllerAdvice
 public class GlobalControllerAdvice {
 
     @ExceptionHandler(RecordNotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
-    public String handleNotFoundException(RecordNotFoundException exception) {
-        return exception.getMessage();
+    public ResponseEntity<String> handleNotFoundException(RecordNotFoundException exception) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(exception.getMessage());
     }
 
     @ExceptionHandler(DuplicateRecordException.class)
-    @ResponseStatus(HttpStatus.CONFLICT)
-    public String handleDuplicateException(DuplicateRecordException exception) {
-        return exception.getMessage();
+    public ResponseEntity<String> handleDuplicateException(DuplicateRecordException exception) {
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(exception.getMessage());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<String> handleIllegalArgumentException(MethodArgumentNotValidException exception) {
+        return ResponseEntity.badRequest().body(InvalidArgumentException.returnMessage(exception));
     }
 
 }
